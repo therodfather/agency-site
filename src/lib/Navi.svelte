0046eacc06e0d91bc99dc9fb1a153f98f92ef1bb
Byte Cats <devin@bytecats.codes>
2023-06-08 15:40:00 -0400

    give us svelte upgrades

---
+++ src/lib/Navi.svelte
@@ -0,0 +1,35 @@
+<nav>
+    <ul class="nav-bar">
+        <li><a href="/">Home</a></li>
+        <li><a href="/software">Software</a></li>
+        <li><a href="/devops">DevOps</a></li>
+        <li><a href="/cloud">Cloud</a></li>
+        <li><a href="/project">Project</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        background: #333;
+        padding: 0.5em;
+    }
+
+    .nav-bar {
+        display: flex;
+        min-width: 100%;
+        width: 100%;
+        justify-content: center; /* Change to "flex-start" to align items from left to right */
+        list-style: none;
+        padding: 1rem;
+        margin: 1rem;
+        font-size: x-large;
+    }
+
+    .nav-bar li {
+        margin: 0 0.5em;
+    }
+
+    .nav-bar li a {
+        color: white;
+        text-decoration: none;
+    }</style>
