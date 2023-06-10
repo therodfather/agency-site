6df4a4c78b6449b466e4fad281aeefe672d7a217
Byte Cats <devin@bytecats.codes>
2023-06-10 17:31:07 -0400

    cookie stuff

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,148 +1,96 @@
-<!-- src/lib/Login.svelte -->
 <script>
-    import { isLoading, loginError } from './store.js';
-    import { login } from './auth'
+    import { login } from "./auth";
+    let isLoading = false;
+    let loginError = false;
 
-    let email = '';
-    let password = '';
-
-    isLoading.set(false);
-
+    let email = "";
+    let password = "";
 </script>
 
-<div class="login-container">
-    <form on:submit|preventDefault={login}>
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" id="email" bind:value={email} placeholder="Enter your email" required />
-    </div>
-    <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" bind:value={password} placeholder="Enter your password" required />
-    </div>
-    <button type="submit" disabled={ isLoading }>
-        {#if isLoading }
-        <span class="spinner"></span>
+<form on:submit|preventDefault={login}>
+    <label>
+        Email:
+        <input type="email" bind:value={email} required />
+    </label>
+    <label>
+        Password:
+        <input type="password" bind:value={password} required />
+    </label>
+    <button type="submit" disabled={isLoading}>
+        {#if isLoading}
+            <span class="spinner" />
         {:else}
-        Login
+            Login
         {/if}
     </button>
-    {#if loginError }
-        <p class="error-message">Invalid email or password. Please try again.</p>
+    {#if loginError}
+        <p class="error-message">
+            Invalid email or password. Please try again.
+        </p>
     {/if}
-
-    </form>
-
-</div>
+</form>
 
 <style>
-    .login-container {
+    label {
         display: flex;
         flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        min-height: 50vh;
-        padding: 1em;
-        color: black;
-        font-family: Arial, sans-serif;
+        margin-bottom: 1em;
     }
 
-    h1 {
-        font-size: 2.5em;
+    input {
+        padding: 0.5em;
+        margin-right: 0.5rem;
+        font-size: 1em;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+    button {
+        padding: 0.7em 1.2em;
+        font-size: 1em;
         font-weight: bold;
-        margin-bottom: 1em;
+        background-color: #4f98ca;
+        color: #ffffff;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    button:disabled {
+        opacity: 0.5;
+        cursor: not-allowed;
+        margin: 0.5em 0;
+    }
+
+    button:hover {
+        background-color: #0d47a1;
+    }
+
+    .spinner {
+        border: 2px solid rgba(255, 255, 255, 0.2);
+        border-top-color: #ffffff;
+        border-radius: 50%;
+        width: 1em;
+        height: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        animation: spin 0.6s linear infinite;
+    }
+
+    @keyframes spin {
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
+    .error-message {
+        color: red;
+        margin-top: 1em;
         text-align: center;
     }
-
-    form {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        width: 300px;
-        background-color: rgba(255, 255, 255, 0.60);
-        border-radius: 8px;
-        padding: 3em;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    }
-
-.form-group {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 1em;
-    width: 100%;
-}
-
-label {
-    font-size: 1.2em;
-    font-weight: bold;
-    margin-bottom: 0.5em;
-    color: rgba(26, 26, 26, 0.80);
-}
-
-input {
-    width: 100%;
-    padding: 0.5em;
-    margin-right: 0.5rem;
-    font-size: 1em;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    background-color: #605d5d;
-    color: #ffffff;
-
-}
-
-  button {
-    padding: 0.7em 1.2em;
-    font-size: 1em;
-    font-weight: bold;
-    background-color: #4f98ca;
-    color: #ffffff;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-    position: relative;
-    display: flex; 
-    justify-content: center;
-    align-items: center;
-  }
-
-  button:disabled {
-    opacity: 0.5;
-    cursor: not-allowed;
-    margin: 0.5em 0;
-  }
-
-
-  button:hover {
-    background-color: #0d47a1;
-}
-
-.spinner {
-    border: 2px solid rgba(255, 255, 255, 0.2);
-    border-top-color: #ffffff;
-    border-radius: 50%;
-    width: 1em;
-    height: 1em;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    animation: spin 0.6s linear infinite;
-    display: flex; /* add this line */
-    justify-content: center; /* add this line */
-    align-items: center; /* add this line */
-  }
-
-@keyframes spin {
-    to {
-    transform: rotate(360deg);
-    }
-}
-
-.error-message {
-    color: red;
-    margin-top: 1em;
-    text-align: center;
-}
-
 </style>
